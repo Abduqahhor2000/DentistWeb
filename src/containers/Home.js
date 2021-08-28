fd0f36eb989b4fe2800e1f20c674470f5029c391
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2021-08-28 20:26:08 +0500

    set css 4

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/home.css";
-import { MdDashboard,
-    BsCreditCard,
-    BiHelpCircle,
-    FiFileText, 
+import { ImExit,
     FiSettings,
     BsBell,
     FiSearch,
     GiHamburgerMenu,
+    AiFillPhone,
+    RiHealthBookLine,
+    FiTwitch,
+    AiOutlineHome,
    } from 'react-icons/all';
 import { Redirect, Route, Switch, Link } from 'react-router-dom';
 import routes from '../routes';
@@ -18,6 +19,7 @@ export default function Home () {
   const userID = useSelector(state => state.user.token);
   const user = useSelector(state => state.user.user);
   const dispatch = useDispatch();
+  const [menuButton, setMenuButton] = useState(false);
 
   const signOutClick = () => {
    dispatch( clearUserID() );
@@ -25,28 +27,30 @@ export default function Home () {
 
     return (
        <div className="home">
-             <div className="menu">
-          <div className="brand"> 
-                <GiHamburgerMenu className="hamburgerMenu" />
+        <div className={`menu ${menuButton ? "menu_is_open" : "menu_is_close"}`}>
+          <div className={`brand ${menuButton ? "" : "brand_close"}`}> 
+                <span className={`dreamDental`}>Dream Dental</span>
+                <ImExit onClick={() => {setMenuButton(!menuButton); return}} className={`hamburgerMenu ${!menuButton ? "Display_none" : ""}`} style={{marginRight: "3px"}} />
+                <GiHamburgerMenu onClick={() => {setMenuButton(!menuButton); return}} className={`hamburgerMenu ${menuButton ? "Display_none" : ""}`} />
           </div>
           <div className="menu-list">
               <ul>
-                  <Link className="router-link" to="/dashboard"><li className=""><MdDashboard className="menu-icons" /> <span>Home</span></li></Link>
-                  <Link className="router-link" to="/balance"><li className="active-list"><BsCreditCard className="menu-icons" /> <span>Blog</span></li></Link>
-                  <Link className="router-link" to="/help"><li className=""><BiHelpCircle className="menu-icons" /> <span>Services</span></li></Link>
-                  <Link className="router-link" to="/invoice"><li className=""><FiFileText className="menu-icons" /> <span>Cantact us</span></li></Link>
+                  <Link className="router-link" to="/dashboard"><li className=""><AiOutlineHome className="menu-icons" /> <span>Biz bilan bog'lanish</span></li></Link>
+                  <Link className="router-link" to="/balance"><li className="active-list"><FiTwitch className="menu-icons" /> <span>Blog</span></li></Link>
+                  <Link className="router-link" to="/help"><li className=""><RiHealthBookLine className="menu-icons" /> <span>Services</span></li></Link>
+                  <Link className="router-link" to="/invoice"><li className=""><AiFillPhone className="menu-icons" /> <span>Cantact us</span></li></Link>
                   <Link className="router-link" to="/setting"><li className=""><FiSettings className="menu-icons" /> <span>Setting</span></li></Link>
               </ul>
           </div>
         </div>
-        <div className="mean">
+        <div className={`mean ${menuButton ? "with_menu" : "menu_less"}`}>
           <div className="mean-header">
-            <div className="search">
+            {/* <div className="search">
               <span>
                 <FiSearch />
               </span>
               <input type="text" placeholder="Search something..."/>
-            </div>
+            </div> */}
             <div className="user-panel">
               {
                 (userID) ? <><div className="bell-icon">
@@ -60,7 +64,8 @@ export default function Home () {
                               </div>
                             </div></> :
                             <>
-                           
+                              <Link className="sign sign-in" to="/sign-in"> Sign In </Link>
+                              <Link className="sign sign-up" to="/sign-up"> Sign Up </Link>
                             </>
               }
              
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./style/normalize.css";
 import "antd/dist/antd.css";
 import './index.css';
 import App from './App';
