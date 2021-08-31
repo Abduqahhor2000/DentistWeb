44e40ad4e1b654fd7086bd2872acd738d611affb
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2021-08-31 11:23:36 +0500

    add blog component start

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,11 +1,31 @@
 import React from "react";
 import "../style/blog.css"
+import BlogComponent from "../styled_components/blog";
 
 export default function Blog() {
 
     return(
-        <div className="coming-soon">
-            <h1>Coming soon...</h1>
-        </div>
-    )
+    // <div>
+        <BlogComponent.LastArticle>
+            <h1>Oxirgi maqola</h1>
+            <div className="card">
+                <div className="photo">
+
+                </div>
+                <div className="mean">
+                    <div className="title">
+                        <h2>Salomatlik bu harakat garovi. shunday ekan. yotib damingizni oling.</h2>
+                    </div>
+                    <div className="text">
+                        <p>
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like...
+                        </p>
+                    </div> 
+                    <div className="status">
+                    </div>
+                </div>
+            </div>
+        </BlogComponent.LastArticle>
+    // </div>
+    );      
 }
---
+++ src/styled_components/blog.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const BlogComponent = {};
+
+BlogComponent.LastArticle = styled.section`
+   .mean{
+       width: 100px;
+   }
+`;
+
+
+export default BlogComponent;
