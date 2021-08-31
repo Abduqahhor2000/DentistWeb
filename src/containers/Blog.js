7ee4f55f86d0b92021726bd70fe86e41056cb1d1
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2021-08-31 18:39:45 +0500

    create glob page

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,31 +1,51 @@
 import React from "react";
 import "../style/blog.css"
 import BlogComponent from "../styled_components/blog";
+import {BsFillHeartFill, BsHeart, FaShareAlt, BsEye} from "react-icons/all";
 
 export default function Blog() {
 
     return(
-    // <div>
+    <div className="isAllBlog">
         <BlogComponent.LastArticle>
-            <h1>Oxirgi maqola</h1>
-            <div className="card">
-                <div className="photo">
-
-                </div>
-                <div className="mean">
-                    <div className="title">
-                        <h2>Salomatlik bu harakat garovi. shunday ekan. yotib damingizni oling.</h2>
+            <div className="blog_card">
+                <h1>Oxirgi maqola</h1>
+                <div className="card">
+                    <div className="photo">
+                        <img src="https://media.istockphoto.com/photos/healthcare-and-medicine-concept-picture-id1147579605" />
                     </div>
-                    <div className="text">
-                        <p>
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like...
-                        </p>
-                    </div> 
-                    <div className="status">
+                    <div className="blog_mean">
+                        <div className="title">
+                            <h2>Salomatlik bu harakat garovi. shunday ekan. yotib damingizni oling.</h2>
+                        </div>
+                        <div className="status">
+                            <div className="info">
+                                <span className="date">31.08.2021</span>
+                                <span><BsEye className="view"/> 142</span>
+                            </div>
+                            <div className="like_share">
+                                <div>
+                                    <BsFillHeartFill className="like liked Display_none"/>
+                                    <BsHeart className="like"/>
+                                    <span> 73</span>
+                                </div>
+                                <div>
+                                    <FaShareAlt className="share"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="text">
+                            <p>
+                                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like...
+                            </p>
+                        </div> 
                     </div>
-                </div>
+                </div>       
             </div>
         </BlogComponent.LastArticle>
-    // </div>
+        <div>
+            dsdddddddddddddddddddddddd
+        </div>
+    </div>
     );      
 }
--- src/styled_components/blog.js
+++ src/styled_components/blog.js
@@ -3,9 +3,122 @@ import styled from "styled-components";
 const BlogComponent = {};
 
 BlogComponent.LastArticle = styled.section`
-   .mean{
-       width: 100px;
+    & {
+       width: 900px;
+       min-width: 300px;
+       height: auto;
+       display: flex;
+       justify-content: flex-start;
+       flex-direction: column;
+       align-items: center;
+
+       @media (max-width: 1170px){
+            width: 500px;
+       }
    }
+
+   h1{
+       font-size: 24px;
+       margin: 20px 0 10px;
+       font-family: 'Acme', sans-serif;
+   }
+   
+   .card {
+       width: 100%;
+       min-width: 300px;
+       height: auto;
+       background-color: white;
+       display: flex;
+       justify-content: space-between;
+       align-items: flex-start;
+       flex-wrap: wrap;
+       border-radius: 5px;
+       overflow: hidden;
+       box-shadow: 0px 5px 6px -3px rgba(0, 0, 0, 0.2);
+
+       @media (max-width: 1170px){
+           width: 450px;
+           flex-direction: column;
+       }
+   }
+
+    .blog_mean {
+        width: 450px;
+        height: 300px;
+        padding: 10px;
+        align-self: center;
+
+        h2 {
+            margin: 0;
+            line-height: 22px;
+            margin-bottom: 5px;
+        }
+
+    @media (max-width: 1170px){
+        height: auto;
+    }
+   }
+
+   .photo{
+       width: 450px;
+       min-width: 300px;
+       height: 300px;
+       background-color: #3b3b3b;
+
+        img {
+            display: block;
+            position: relative;
+            width: 100%;
+            height: 100%;
+            background-color: rgb(15, 37, 37);
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    }
+
+    .status{
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        color: #b3b3b3;
+        margin-bottom: 10px;
+    }
+    .info{
+        width: 130px;
+        display: flex;
+        justify-content: space-between;
+        align-items:center;
+    }
+
+    .view{
+        font-size: 16px;
+        color: #979797;
+        position: relative;
+        top: 2px;
+    }
+
+    .like_share{
+        width: 100px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    .like{
+        font-size: 16px;
+        color: #ff4949;
+        position: relative;
+        top: 4px;
+    }
+
+    .share{
+        width: 25px;
+        font-size: 16px;
+        color: #1d59ce;
+        position: relative;
+        top: 4px;
+    }
 `;
 
 
