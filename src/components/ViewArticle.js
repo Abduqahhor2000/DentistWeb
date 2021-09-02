e4d978ecf1dc6f3dae92ec3e3256f9b1720d59a9
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2021-09-02 22:55:17 +0500

    created blog page 2.7

--- src/components/ViewArticle.js
+++ src/components/ViewArticle.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import BlogComponent from "../styled_components/blog";
 import { useParams } from "react-router";
 import { useSelector } from "react-redux";
 
 export default function ViewArticle () {
     const params = useParams();
+    const [converted, setCanverted] = useState([]);
 
-    const articleuseSelector
+    const article = useSelector((state) => { 
+        return  state.articles.articles.find( (item) => {
+                    if(item.id === params.id){
+                        return true;
+                    }
+                    return false;
+                })
+    });
+    
+    const converting = () => {
+        setCanverted([...converted, ]);
+    }
 
 
     
